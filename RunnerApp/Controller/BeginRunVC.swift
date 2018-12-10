e6a8a16c7c5a98f8846a4949d281d8150f0f1a06
Alexandr <Sashob84@gmail.com>
2018-12-10 15:31:56 +0200

    Fetch Realm object by ID(Run model)

--- RunnerApp/Controller/BeginRunVC.swift
+++ RunnerApp/Controller/BeginRunVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import RealmSwift
 
 class BeginRunVC: LocationVC {
 
@@ -22,43 +23,88 @@ class BeginRunVC: LocationVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationAuthStatus()
-        mapView.delegate = self
-        print("\(Run.getAllRuns())")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
+        mapView.delegate = self
         manager?.startUpdatingLocation()
-        getLastRun()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupMapView()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
     
-    func getLastRun() {
-        guard let lastRun = Run.getAllRuns()?.first else {
+    func setupMapView() {
+        if let overlay = addLastRunToMap() {
+            if mapView.overlays.count > 0 {
+                mapView.removeOverlays(mapView.overlays)
+            }
+            mapView.addOverlay(overlay)
+            lastRunStack.isHidden = false
+            lastRunBGView.isHidden = false
+            lastRunClose.isHidden = false
+        } else {
             lastRunStack.isHidden = true
             lastRunBGView.isHidden = true
             lastRunClose.isHidden = true
-            return
+            centerMapOnUserLocation()
         }
-        lastRunStack.isHidden = false
-        lastRunBGView.isHidden = false
-        lastRunClose.isHidden = false
+    }
+    
+    func addLastRunToMap() -> MKPolyline? {
+        guard let lastRun = Run.getAllRuns()?.first else {return nil}
         paceLbl.text = lastRun.pace.formatTimeDurationToString()
         distanceLbl.text = "\(lastRun.distance.metersToMiles(places: 2)) mi"
         durationLbl.text = lastRun.duration.formatTimeDurationToString()
+        
+        var coordinate = [CLLocationCoordinate2D]()
+        for location in lastRun.locations {
+            coordinate.append(CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+        }
+        
+        mapView.userTrackingMode = .none
+        guard let locations = Run.getRun(byID: lastRun.id)?.locations else {return MKPolyline()}
+        mapView.setRegion(centerMapOnPrevRoute(locations: locations), animated: true)
+        
+        return MKPolyline(coordinates: coordinate, count: locations.count)
+    }
+    
+    func centerMapOnUserLocation() {
+        mapView.userTrackingMode = .follow
+        let coordinateRegion = MKCoordinateRegion.init(center: mapView.userLocation.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func centerMapOnPrevRoute(locations: List<Location>) -> MKCoordinateRegion {
+        guard let initialLocation = locations.first else {return MKCoordinateRegion()}
+        var minLatitude = initialLocation.latitude
+        var minLongitude = initialLocation.longitude
+        var maxLatitude = minLatitude
+        var maxLongitude = maxLatitude
+        
+        for location in locations {
+            minLatitude = min(minLatitude, location.latitude)
+            minLongitude = min(minLongitude, location.longitude)
+            maxLatitude = max(maxLatitude, location.latitude)
+            maxLongitude = max(maxLongitude, location.longitude)
+        }
+        return MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: (minLatitude + maxLatitude) / 2, longitude: (minLongitude + maxLongitude) / 2), span: MKCoordinateSpan(latitudeDelta: (maxLatitude - minLatitude) * 1.4, longitudeDelta: (maxLongitude - minLongitude) * 1.4))
     }
     
     @IBAction func lastRunCloseBtn(_ sender: Any) {
         lastRunStack.isHidden = true
         lastRunBGView.isHidden = true
         lastRunClose.isHidden = true
+        centerMapOnUserLocation()
     }
     
     @IBAction func locationBtnPressed(_ sender: Any) {
-        
+        centerMapOnUserLocation()
     }
 }
 
@@ -67,7 +113,15 @@ extension BeginRunVC: CLLocationManagerDelegate {
         if status == .authorizedWhenInUse {
             checkLocationAuthStatus()
             mapView.showsUserLocation = true
-            mapView.userTrackingMode = .follow
         }
     }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let polyline = overlay as! MKPolyline
+        let renderer = MKPolylineRenderer(polyline: polyline)
+        renderer.strokeColor = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
+        renderer.lineWidth = 4
+        return renderer
+    }
+    
 }
